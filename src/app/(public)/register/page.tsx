e1517e9fc781f3ce34ced77053c02c76639fbcbb
andreodev <developerandreo@gmail.com>
2025-04-06 17:37:16 -0300

    erro depl

--- src/app/(public)/register/page.tsx
+++ src/app/(public)/register/page.tsx
@@ -10,7 +10,7 @@ export default async function RegisterPage(){
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <Analytics />
-      <Register />
+        <Register />
 
       </div>
     </div>

f265c76d136040a55efb53c5b6f826f66182ff2b
andreodev <developerandreo@gmail.com>
2025-04-07 11:01:05 -0300

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
-
-
+import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -13,7 +11,24 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "Blog Pessoal",
-  description: "Desenvolvido por Andreo Henrique.",
+  description: "Desenvolvido por Andreo Henrique, um espaço para compartilhar conhecimentos sobre desenvolvimento, tecnologia e programação.",
+  keywords: "blog, desenvolvimento, tecnologia, programação, Andreo Henrique, dev, curiosidades, dicas de código",
+  authors: [{ name: "Andreo Henrique" }],
+  robots: "index, follow",
+  openGraph: {
+    title: "Blog Pessoal de Andreo Henrique",
+    description: "Um blog dedicado a temas como desenvolvimento de software, programação, e mais.",
+    url: "https://blog-viewreo.vercel.app/",  // Altere para o URL do seu blog
+    images: ['/assets/pj5.png'], // Adicione uma imagem para o Open Graph
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@andreodev", // Alterar para seu Twitter se necessário
+    title: "Blog Pessoal de Andreo Henrique",
+    description: "Desenvolvido por Andreo Henrique, um espaço para compartilhar conhecimentos sobre desenvolvimento, tecnologia e programação.",
+    images: ['/assets/pj5.png'], // Imagem para Twitter Card
+  },
 };
 
 export default function RootLayout({

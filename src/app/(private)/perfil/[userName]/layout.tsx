f09a8728a8374ae72581b6872b62c7add880833a
andreodev <developerandreo@gmail.com>
2025-04-07 11:05:19 -0300

    deploy

--- src/app/(private)/perfil/[userName]/layout.tsx
+++ src/app/(private)/perfil/[userName]/layout.tsx
@@ -5,7 +5,6 @@ import { SessionProvider } from "next-auth/react";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import { Analytics } from "@vercel/analytics/react"
-import type { Metadata } from "next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,27 +21,6 @@ const inter = Inter({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Blog Pessoal",
-  description: "Desenvolvido por Andreo Henrique, um espaço para compartilhar conhecimentos sobre desenvolvimento, tecnologia e programação.",
-  keywords: "blog, desenvolvimento, tecnologia, programação, Andreo Henrique, dev, curiosidades, dicas de código",
-  authors: [{ name: "Andreo Henrique" }],
-  robots: "index, follow",
-  openGraph: {
-    title: "Blog Pessoal de Andreo Henrique",
-    description: "Um blog dedicado a temas como desenvolvimento de software, programação, e mais.",
-    url: "https://blog-viewreo.vercel.app/",  // Altere para o URL do seu blog
-    images: ['/assets/pj5.png'], // Adicione uma imagem para o Open Graph
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@andreodev", // Alterar para seu Twitter se necessário
-    title: "Blog Pessoal de Andreo Henrique",
-    description: "Desenvolvido por Andreo Henrique, um espaço para compartilhar conhecimentos sobre desenvolvimento, tecnologia e programação.",
-    images: ['/assets/pj5.png'], // Imagem para Twitter Card
-  },
-};
 
 export default function ProfileRoot({
   children,
